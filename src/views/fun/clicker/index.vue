4b9fcf80424b55d17e474273fd2930d608e25e59
ESEW <2124683945@qq.com>
2025-12-09 16:28:22 +0800

    新增 部分设置功能

--- src/views/fun/clicker/index.vue
+++ src/views/fun/clicker/index.vue
@@ -1,14 +1,24 @@
 <template>
   <div class="main">
     <div class="topBar">
-      <div class="w-33%"></div>
+      <div class="w-33% flex gap-10px items-center c-#333">
+        <span>时长:</span>
+        <el-input class="w-60px" v-model="config.time" placeholder="游戏时长" />
+        <span>初始速度:</span>
+        <el-input class="w-60px" v-model="config.speed" placeholder="游戏时长" />
+      </div>
       <div class="w-33% flex justify-center gap-10px">
         <el-tag size="large" type="success">得分:{{ Math.floor(point) }}</el-tag>
-        <el-tag size="large" type="warning">速度{{ (speed / 1000).toFixed(2) }}s/个</el-tag>
-        <el-tag size="large" type="danger"> 剩余时间:{{ time }} </el-tag>
+        <el-tag size="large" type="warning"
+          >速度{{
+            starting ? (speed / 1000).toFixed(2) : (config.speed / 1000).toFixed(2)
+          }}s/个</el-tag
+        >
+        <el-tag size="large" type="danger"> 剩余时间:{{ starting ? time : config.time }}s </el-tag>
       </div>
       <div class="w-33% flex justify-end">
-        <el-button @click="startGame()">开始</el-button>
+        <el-button>规则</el-button>
+        <el-button type="success" :disabled="starting" @click="startGame()">开始</el-button>
       </div>
     </div>
 
@@ -27,20 +37,25 @@
   </div>
 </template>
 <script setup>
-import { onMounted, ref } from 'vue'
+import { onMounted, reactive, ref } from 'vue'
 import FunCircle from './components/FunCircle.vue'
+
+const config = reactive({
+  time: 60,
+  speed: 1000,
+})
 
 const gameRef = ref(null)
 const height = ref()
 const width = ref()
 const circleList = ref([])
 
-const speed = ref(1000)
+const speed = ref()
 let radious = 100
-const time = ref(60)
+const time = ref()
 const point = ref(0)
 
-let starting = false
+const starting = ref(false)
 
 const timer = () => {
   time.value--
@@ -75,17 +90,17 @@ const addCircle = () => {
       }
       addCircle()
     } else {
-      starting = false
+      starting.value = false
     }
   }, speed.value)
 }
 
 const startGame = () => {
-  if (!starting) {
-    starting = true
-    speed.value = 1000
+  if (!starting.value) {
+    starting.value = true
+    speed.value = config.speed
     radious = 100
-    time.value = 60
+    time.value = config.time
     point.value = 0
     timer()
     addCircle()
@@ -100,9 +115,8 @@ onMounted(() => {
   width.value = gameRef.value.clientWidth
   height.value = gameRef.value.clientHeight
 
-  console.log({
-    场地: [width.value, height.value],
-  })
+  time.value = config.time
+  speed.value = config.speed
 })
 </script>
 
