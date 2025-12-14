24ecd8c1c459149683ab7a673ef0fac058541eea
ESEW <2124683945@qq.com>
2025-12-14 23:59:53 +0800

    111

--- src/views/chartRoom/config.js
+++ src/views/chartRoom/config.js
@@ -2,7 +2,9 @@ const image = '/images/logo.png'
 
 export const ChartRoomMessage = {
   userInfo: {
-    name: 'wang',
+    username: 'wang',
+    account: '',
+    acatar: image,
   },
   messageRoomList: [
     {
@@ -56,6 +58,27 @@ export const ChartRoomMessage = {
       avatar: image,
       name: 'wang',
       message: '🥰',
+    },
+  ],
+}
+
+export const ChartIndex = {
+  carousel: [
+    {
+      text: `Div杀手:
+            使用鼠标操控自己的div，除了躲避大家伙的吞噬,记得消灭那些小家伙。
+            有一个永远比你强大的boss，记得远离它。`,
+      image: '/images/index/1.png',
+    },
+    {
+      text: `点点点：
+使用你的鼠标点击哪些不断出现在屏幕中的黑色圆圈，它们会给予你一些得积分。
+黑色圆圈的出现速度会越来越快，面积也会越来越小，直到它们达到我预定的阈值（当然，你也可以决定一部分）。`,
+      image: '/images/index/2.png',
+    },
+    {
+      text: `bilibili山寨版:尝试对哔哩哔哩进行复刻,目前计划复刻首页以及视频详情页`,
+      image: '/images/index/3.png',
     },
   ],
 }
