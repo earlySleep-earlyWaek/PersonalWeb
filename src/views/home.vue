f2aa307a385415d8247083d95f45a58655eaf291
ESEW <2124683945@qq.com>
2026-01-04 17:41:12 +0800

    chore(home): 更新Bili山寨版标签状态
    - 将Bili山寨版标签的开发状态从"开发中"更改为"暂停"

--- src/views/home.vue
+++ src/views/home.vue
@@ -4,7 +4,7 @@
       <el-tab-pane name="welcome" label="欢迎" />
       <el-tab-pane name="fun" label="小创意" />
       <el-tab-pane name="chartRoom" label="聊天室(开发中)" />
-      <el-tab-pane name="bilibili" label="Bili山寨版(开发中)" />
+      <el-tab-pane name="bilibili" label="Bili山寨版(暂停)" />
       <el-tab-pane name="map" label="地图(暂停)" />
       <el-tab-pane name="echarts" label="数据统计(预设)" />
       <el-tab-pane name="test" label="组件测试" />
